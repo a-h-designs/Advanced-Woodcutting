e0549a529932be32f23e9c5cf24f902edab65e07
AHDesigns <anthony.hancock85@gmail.com>
2024-03-12 21:38:47 +0000

    Added the basic to make it like the original plugin

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# Advanced Woodcutting
+An Advanced Woodcutting greeter plugin
--- src/test/java/com/advancedwoodcutting/AdvancedWoodcuttingPluginTest.java
+++ src/test/java/com/advancedwoodcutting/AdvancedWoodcuttingPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.advancedwoodcutting;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class AdvancedWoodcuttingPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(AdvancedWoodcuttingPlugin.class);
 		RuneLite.main(args);
 	}
 }
